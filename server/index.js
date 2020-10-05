936022eced3d86d5fee75e4f83bebb58362bbca6
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2020-10-05 23:18:56 +0400

    make it better

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 let users = [];
