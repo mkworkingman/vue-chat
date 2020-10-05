4425dd942145fc1cec77fa143416247f7d05693f
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2020-10-05 23:11:49 +0400

    make it better

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
-const express = require("express")();
-const http = require("http").Server(express);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 let users = [];
 
@@ -58,6 +64,6 @@ io.on('connection', socket => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("Port 3000.");
+http.listen(PORT, () => {
+  console.log("PORT: " + PORT);
 });
